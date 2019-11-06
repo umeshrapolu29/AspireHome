797fdf8dbc223aae0562fbec97baf2d0165ec0d4
umeshrapolu29 <umeshrapolu29@gmail.com>
2019-11-06 17:04:13 +0530

    initial commits

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { FormComponent } from './form/form.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
+    FormComponent
   ],
   imports: [
     BrowserModule,
